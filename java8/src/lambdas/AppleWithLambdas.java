00b3e8165da76f25e1fea43c3e1ab7ee51c8b5a1
Amudhan <aravamudhan.a@gmail.com>
2017-08-11 11:06:49 +0530

    Step 3 - Predicates and Consumers

--- java8/src/lambdas/AppleWithLambdas.java
+++ java8/src/lambdas/AppleWithLambdas.java
@@ -1,31 +1,53 @@
 package lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class AppleWithLambdas {
 
   public static void main(String[] args) {
- // Create a list of apples
+    // Create a list of apples
     List<Apple> inventory = Arrays.asList(new Apple(80, "green"), new Apple(155, "green"),
         new Apple(120, "red"), new Apple(130, "light green"), new Apple(150, "dark red"));
     System.out.println("***** Print the apple types *****");
-    for(Apple item : inventory){
-      item.printApple((Apple a)-> {
+    for (Apple item : inventory) {
+      item.printApple((Apple a) -> {
         String appleType = null;
-        if(a.getWeight()>150){
+        if (a.getWeight() > 150) {
           appleType = "good apple";
-        }else{
+        } else {
           appleType = "bad apple";
         }
-        return "Apple : "+a.getWeight()+"grams. This is "+appleType;
+        return "Apple : " + a.getWeight() + "grams. This is " + appleType;
       });
     }
     System.out.println("***** Print the apples with colors *****");
-    for(Apple item : inventory){
-      item.printApple( (Apple a)-> {return a.getColor()+" apple";});// Both the lines are same
-      item.printApple( a-> a.getColor()+" apple"); 
+    for (Apple item : inventory) {
+      item.printApple((Apple a) -> {
+        return a.getColor() + " apple";
+      });// Both the lines are same
+      item.printApple(a -> a.getColor() + " apple");
     }
+    System.out.println("***** Filter apples based on criteria *****");
+    // Just get me red apples
+    List<Apple> redApples =
+        appleFilter(inventory, a -> a.getColor().equalsIgnoreCase("red"));
+    // Using the forEach(Consumer<? extends Apple>action)
+    redApples.forEach(a->System.out.println(a.toString()));
+
   }
-  
+
+  // This method filters apples based on how Predicate#test is implemented
+  static List<Apple> appleFilter(List<Apple> apples, Predicate<Apple> p) {
+    List<Apple> filteredApples = new ArrayList<>();
+    for (Apple apple : apples) {
+      if (p.test(apple)) {
+        filteredApples.add(apple);
+      }
+    }
+    return filteredApples;
+  }
+
 }
--- java8/src/lambdas/StrategyDesignPattern.java
+++ java8/src/lambdas/StrategyDesignPattern.java
@@ -1,5 +1,6 @@
 package lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,30 +9,42 @@ public class StrategyDesignPattern {
   public static void main(String[] args) {
     List<Apple> inventory = Arrays.asList(new Apple(80, "green"), new Apple(155, "green"),
         new Apple(120, "red"), new Apple(200, "dark red"), new Apple(220, "lite green"));
-    for(Apple apple : inventory){
+    for (Apple apple : inventory) {
       apple.printApple(new AppleColorPrint());
       apple.printApple(new AppleWeightPrint());
       apple.printApple(new ApplePrettyPrint());
     }
-    for(Apple apple : inventory){
-      apple.printApple(new AppleFormatter(){
+    for (Apple apple : inventory) {
+      apple.printApple(new AppleFormatter() {
 
         @Override
         public String format(Apple apple) {
           String appleType = null;
-          if(apple.getWeight()>150){
+          if (apple.getWeight() > 150) {
             appleType = "good apple";
-          }else{
+          } else {
             appleType = "bad apple";
           }
-          return "Apple : "+apple.getWeight()+"grams. This is "+appleType;
+          return "Apple : " + apple.getWeight() + "grams. This is " + appleType;
         }
-        
+
       });
+    }
+    // Get me red apples
+    List<Apple> redApples = new ArrayList<>();
+    for (Apple a : inventory) {
+      if ("red".equalsIgnoreCase(a.getColor())) {
+        redApples.add(a);
+      }
+    }
+    for (Apple a : redApples) {
+      a.printApple(new ApplePrettyPrint());
     }
   }
 
 }
+
+
 class Apple {
   private Integer weight = 0;
   private String color = "";
@@ -65,7 +78,7 @@ class Apple {
   public void setColor(String color) {
     this.color = color;
   }
-  
+
   @Override
   public String toString() {
     return "Apple [weight=" + weight + ", color=" + color + "]";
@@ -75,9 +88,12 @@ class Apple {
     System.out.println(formatter.format(this));
   }
 }
+
+
 interface AppleFormatter {
   public String format(Apple apple);
 }
+
 
 class AppleColorPrint implements AppleFormatter {
   @Override
@@ -87,10 +103,11 @@ class AppleColorPrint implements AppleFormatter {
   }
 }
 
+
 class AppleWeightPrint implements AppleFormatter {
   @Override
   public String format(Apple apple) {
-    String s = "Apple thats weights : "+apple.getWeight()+" grams";
+    String s = "Apple thats weights : " + apple.getWeight() + " grams";
     return s;
   }
 }
@@ -99,8 +116,8 @@ class AppleWeightPrint implements AppleFormatter {
 class ApplePrettyPrint implements AppleFormatter {
   @Override
   public String format(Apple apple) {
-    String s = "The apple is of " + apple.getColor() + " color, weighing " + apple.getWeight()
-        + " grams";
+    String s =
+        "The apple is of " + apple.getColor() + " color, weighing " + apple.getWeight() + " grams";
     return s;
   }
 }

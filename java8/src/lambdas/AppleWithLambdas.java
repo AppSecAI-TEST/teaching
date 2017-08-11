a2ddad43a504b4dde6fe11048811d42b39ace821
Amudhan <aravamudhan.a@gmail.com>
2017-08-11 11:17:36 +0530

    Step 4 - Filtering using predicates, consumer basics

--- java8/src/lambdas/AppleWithLambdas.java
+++ java8/src/lambdas/AppleWithLambdas.java
@@ -3,6 +3,7 @@ package lambdas;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class AppleWithLambdas {
@@ -30,13 +31,34 @@ public class AppleWithLambdas {
       });// Both the lines are same
       item.printApple(a -> a.getColor() + " apple");
     }
+    // Create a lambda expression to display apples in the console
+    // This uses the Consumer interface
+    Consumer<List<Apple>> applesConsoleDisplay = (appleList) -> {
+      for (Apple apple : appleList) {
+        System.out.println(apple);
+      }
+    };
+    System.out.println("-----All apples-----");
+    consumeList(inventory, applesConsoleDisplay);
     System.out.println("***** Filter apples based on criteria *****");
     // Just get me red apples
-    List<Apple> redApples =
-        appleFilter(inventory, a -> a.getColor().equalsIgnoreCase("red"));
+    // Here we are using the Predicate interface provided by the JDK. This interface is called
+    // functional interface
+    List<Apple> redAppleList = appleFilter(inventory, a -> a.getColor().equalsIgnoreCase("red"));
     // Using the forEach(Consumer<? extends Apple>action)
-    redApples.forEach(a->System.out.println(a.toString()));
-
+    redAppleList.forEach(a -> System.out.println(a.toString()));
+    // Method references
+    redAppleList.forEach(System.out::println);
+    // Predefined predicates
+    Predicate<Apple> redApples = redApple -> redApple.getColor().equalsIgnoreCase("red");
+    Predicate<Apple> greenApples = greenApple -> greenApple.getColor().equalsIgnoreCase("green");
+    Predicate<Apple> moreThan150 = heavyApple -> heavyApple.getWeight() > 150;
+    System.out.println("*** Getting NOT GREEN apples **");
+    appleFilter(inventory, greenApples.negate()).forEach(System.out::println);
+    System.out.println("*** Getting apples that are either GREEN or RED **");
+    appleFilter(inventory, redApples.or(greenApples)).forEach(System.out::println);
+    System.out.println("*** Getting apples that are GREEN and more than 150kg**");
+    appleFilter(inventory, greenApples.and(moreThan150)).forEach(System.out::println);
   }
 
   // This method filters apples based on how Predicate#test is implemented
@@ -49,5 +71,11 @@ public class AppleWithLambdas {
     }
     return filteredApples;
   }
+
+  // This method accepts a list and a consumer and perform operations implemented on the consumer
+  static <T> void consumeList(List<T> list, Consumer<List<T>> consumer) {
+    consumer.accept(list);
+  }
+
 
 }
